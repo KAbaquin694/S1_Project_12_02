ab656964fa0ee71a5be6eff1f94668275271cd38
Khalel Abaquin <kabaquin694@west-mec.org>
2019-02-28 15:27:57 -0700

    Update 2
    placed the 'article' & 'aside' element before the 'section' closing tag, added variables, added 3 for loops, & added 2 'getElementsByTagNames' methods (something is broken)

--- hg_report.js
+++ hg_report.js
@@ -11,3 +11,28 @@
    Filename: hg_report.js
 	
 */
+
+var gameReport = "<h1>" + itemTitle + "</h1> <h2>By: " + itemManufacturer + "</h2> <img src=”hg_id.png” alt=”id” id=”gameImg” /> <table><tr><th>Product ID</th><td>" + itemID + "</td></tr><tr><th>List Price</th><td>" + itemPrice + "</td></tr> <tr><th>Platform</th><td>" + itemPlatform + "</td></tr> <tr><th>ESRB Rating</th><td>" + itemESRB + "</td></tr><tr><th>Condition</th><td>" + itemCondition + "</td></tr> <tr><th>Release</th><td>" + itemRelease + "</td></tr></table>" + itemSummary + "";
+
+document.getElementsByTagName("article").innerHTML = gameReport[0];
+
+var ratingsSum = 0;
+var ratingsCount = ratings.length;
+
+for (var i = 0; i < ratings.length; i++) {
+    ratings[i] + ratingsSum;
+}
+
+var ratingsAvg = ratingsSum / ratingsCount;
+var ratingReport = "<h1>Customer Reviews</h1><h2> " + ratingsAvg + " out of 5 stars (" + ratingsCount + " reviews)</h2>";
+
+for (var i = 0; i < 2; i++) {
+    ratingReport += "div class=”review”> <h1>" + ratingTitles[i] + "</h1><table> <tr><th>By</th><td>" + ratingAuthors[i] + "</td></tr> <tr><th>Review Date</th><td>" + ratingDates[i] + "</td></tr> <tr><th>Rating</th><td>";
+
+    for (var i = 1; i < ratings[i]; i++) {
+        ratingReport += "<img src=”hg_star.png” />";
+    }
+    ratingReport += "</td></tr></table>" + ratingSummaries[i] + "</div>";
+}
+
+document.getElementsByTagName("aside").innerHTML = ratingReport;
