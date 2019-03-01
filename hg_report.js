92059cd6a055b28e58d3c5e060a88a0105340e59
Khalel Abaquin <kabaquin694@west-mec.org>
2019-03-01 14:16:40 -0700

    Final Update (RFR)
    gave the 'article' element an id of 'gameInfo', fixed broken code with help of debuggers (need single tics inside a text string), added/replaced a few operators, replaced the var ' i ' with ' x ', & added final comments

--- hg_report.js
+++ hg_report.js
@@ -12,27 +12,34 @@
 	
 */
 
-var gameReport = "<h1>" + itemTitle + "</h1> <h2>By: " + itemManufacturer + "</h2> <img src=”hg_id.png” alt=”id” id=”gameImg” /> <table><tr><th>Product ID</th><td>" + itemID + "</td></tr><tr><th>List Price</th><td>" + itemPrice + "</td></tr> <tr><th>Platform</th><td>" + itemPlatform + "</td></tr> <tr><th>ESRB Rating</th><td>" + itemESRB + "</td></tr><tr><th>Condition</th><td>" + itemCondition + "</td></tr> <tr><th>Release</th><td>" + itemRelease + "</td></tr></table>" + itemSummary + "";
+// This block of code stores a text string with multiple variables implemented within inside the variable 'gameReport'.
+var gameReport = "<h1>" + itemTitle + "</h1><h2>By: " + itemManufacturer + "</h2><img src='hg_" + itemID + ".png' alt='id' id='gameImg'/> <table><tr><th>Product ID</th><td>" + itemID + "</td></tr><tr><th>List Price</th><td>" + itemPrice + "</td></tr><tr><th>Platform</th><td>" + itemPlatform + "</td></tr><tr><th>ESRB Rating</th><td>" + itemESRB + "</td></tr><tr><th>Condition</th><td>" + itemCondition + "</td></tr><tr><th>Release</th><td>" + itemRelease + "</td></tr></table>" + itemSummary + "";
 
-document.getElementsByTagName("article").innerHTML = gameReport[0];
+// This line of code grabs the element with the id 'gameInfo' & inserts the value of 'gameReport' inside its innerHTML.
+document.getElementById("gameInfo").innerHTML = gameReport;
 
+// This section of code declares the variable of 'ratingsSum' to the number 0 & the variable 'ratingsCount' to the length of the 'ratings' array.
 var ratingsSum = 0;
 var ratingsCount = ratings.length;
 
+// In this block of code, every time it loops through the array items, the value of 'ratingsSum' & 'ratings' are added together.
 for (var i = 0; i < ratings.length; i++) {
-    ratings[i] + ratingsSum;
+    ratingsSum += ratings[i]
 }
 
+// This section of code sets the variable 'ratingsAvg' to the quotient of 'ratingsSum' & 'ratinsCount'. This section also sets the variable 'ratingReport' to a text string with other variables embedded within.
 var ratingsAvg = ratingsSum / ratingsCount;
 var ratingReport = "<h1>Customer Reviews</h1><h2> " + ratingsAvg + " out of 5 stars (" + ratingsCount + " reviews)</h2>";
 
-for (var i = 0; i < 2; i++) {
-    ratingReport += "div class=”review”> <h1>" + ratingTitles[i] + "</h1><table> <tr><th>By</th><td>" + ratingAuthors[i] + "</td></tr> <tr><th>Review Date</th><td>" + ratingDates[i] + "</td></tr> <tr><th>Rating</th><td>";
+// This block of code has two for loops, one being nested within the other to display the reviews (images of stars, title, authors, & dates) of the website.
+for (var i = 0; i <= 2; i++) {
+    ratingReport += "<div class='review'><h1>" + ratingTitles[i] + "</h1><table><tr><th>By</th><td>" + ratingAuthors[i] + "</td></tr><tr><th>Review Date</th><td>" + ratingDates[i] + "</td></tr><tr><th>Rating</th><td>";
 
-    for (var i = 1; i < ratings[i]; i++) {
-        ratingReport += "<img src=”hg_star.png” />";
+    for (var x = 1; x <= ratings[i]; x++) {
+        ratingReport += "<img src='hg_star.png'/>";
     }
     ratingReport += "</td></tr></table>" + ratingSummaries[i] + "</div>";
 }
 
-document.getElementsByTagName("aside").innerHTML = ratingReport;
+// This line of code grabs the 'aside' element & replaces its HTML with the value(s) of 'ratingReport'.
+document.getElementsByTagName("aside")[0].innerHTML = ratingReport;
